cd76e0ba1ca1e3d267b5cd803ca6384e3cab40f6
Charly <beaussart@protonmail.com>
2022-08-30 17:21:59 +0200

    env

--- sqldb/database.py
+++ sqldb/database.py
@@ -7,12 +7,12 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 
-DATABASE_URL_LECLERC = os.getenv("postgresql://WelleatAdminLeclerc:Gaudec42@leclerc-welleat.postgres.database.azure.com:5432/postgres").replace('postgres://','postgresql://')
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace('postgres://','postgresql://')
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123soleil@localhost:5432/leclerc_test"
 # SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace('postgres://','postgresql://')
 
-engine = create_engine(DATABASE_URL_LECLERC)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()

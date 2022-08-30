af5d3ad7bc393f31bf27f462d985c6776c004104
Charly <beaussart@protonmail.com>
2022-08-30 09:22:37 +0200

    second commit

--- sqldb/database.py
+++ sqldb/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
-load_dotenv(override=True)
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(override=True)
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123soleil@localhost:5432/leclerc_test"
 # SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace('postgres://','postgresql://')
--- sqldb/schemas.py
+++ sqldb/schemas.py
@@ -1,5 +1,5 @@
-from typing import List, Union
-from pydantic import BaseModel
+# from typing import List, Union
+# from pydantic import BaseModel
 
 # class Product(BaseModel):
 #     id : int

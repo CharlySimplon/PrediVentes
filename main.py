89632c2bc5f9101f1bdefb89eefe6c417d45b3e4
Charly <beaussart@protonmail.com>
2022-08-30 13:23:19 +0200

    test get 5

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import uvicorn
 from fastapi import FastAPI, APIRouter, Request, Depends
 # from sqlalchemy.orm import Session
 
-from sqldb import crud, models, schemas
-from sqldb.database import SessionLocal, engine, get_db
+# from sqldb import crud, models, schemas
+# from sqldb.database import SessionLocal, engine, get_db
 
 app = FastAPI()
 api_router = APIRouter()
@@ -13,16 +13,18 @@ api_router = APIRouter()
 async def root():
     return {"message": "TEST 2 "}
 
+app.include_router(api_router)
+
 # @api_router.get("/stores/")
 # async def read_entities(db: Session = Depends(get_db)):
 #     entities = crud.get_all_entities(db)
 #     return entities
 
-app.include_router(api_router)
+
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app")
 
 
 
--- sqldb/models.py
+++ sqldb/models.py
@@ -1,77 +1,77 @@
-from sqlalchemy import Integer, ForeignKey, String, Column, Boolean, DateTime, Float
-from .database import Base
-from sqlalchemy.orm import relationship
-import datetime
+# from sqlalchemy import Integer, ForeignKey, String, Column, Boolean, DateTime, Float
+# from .database import Base
+# from sqlalchemy.orm import relationship
+# # import datetime
 
-class Entities(Base):
-    __tablename__ = "Entities"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+# class Entities(Base):
+#     __tablename__ = "Entities"
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String)
 
-    stockings_to_entities = relationship("Stockings", back_populates = "entities_to_stockings")
+#     stockings_to_entities = relationship("Stockings", back_populates = "entities_to_stockings")
 
-class Stockings(Base):
-    __tablename__ = "Stockings"
-    id = Column(Integer, primary_key=True, index=True)
-    EntityId = Column(Integer, ForeignKey("Entities.id"))
+# class Stockings(Base):
+#     __tablename__ = "Stockings"
+#     id = Column(Integer, primary_key=True, index=True)
+#     EntityId = Column(Integer, ForeignKey("Entities.id"))
 
-    entities_to_stockings = relationship("Entities", back_populates = "stockings_to_entities")
-    products_to_stockings = relationship("Products", back_populates = "stockings_to_products")
+#     entities_to_stockings = relationship("Entities", back_populates = "stockings_to_entities")
+#     products_to_stockings = relationship("Products", back_populates = "stockings_to_products")
 
-class Categories(Base):
-    __tablename__ = "Categories"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+# class Categories(Base):
+#     __tablename__ = "Categories"
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String)
 
-    categories_to_product = relationship("Products")
+#     categories_to_product = relationship("Products")
 
-class Products(Base):
-    __tablename__ = "Product"
-    id = Column(Integer, primary_key=True, index=True)
-    CategorieId = Column(Integer, ForeignKey("Categories.id"))
-    name = Column(String)
-    quantity_unit = Column(String)
-    price = Column(Float)
-    pricePerLiterOrKg = Column(Float)
-    lifetime = Column(Integer)
-    StockingId = Column(Integer, ForeignKey("Stockings.id"))
-    stock_min = Column(Float)
-    stock_max = Column(Float)
-    quantity = Column(Float)
-    # "margin" could be great
+# class Products(Base):
+#     __tablename__ = "Product"
+#     id = Column(Integer, primary_key=True, index=True)
+#     CategorieId = Column(Integer, ForeignKey("Categories.id"))
+#     name = Column(String)
+#     quantity_unit = Column(String)
+#     price = Column(Float)
+#     pricePerLiterOrKg = Column(Float)
+#     lifetime = Column(Integer)
+#     StockingId = Column(Integer, ForeignKey("Stockings.id"))
+#     stock_min = Column(Float)
+#     stock_max = Column(Float)
+#     quantity = Column(Float)
+#     # "margin" could be great
 
-    stockings_to_products = relationship("Stockings", back_populates="products_to_stockings")
-    sells_to_products = relationship("ProductSells", back_populates="products_to_sells")
-    trashes_to_products = relationship("Trashes", back_populates="products_to_trashes")
+#     stockings_to_products = relationship("Stockings", back_populates="products_to_stockings")
+#     sells_to_products = relationship("ProductSells", back_populates="products_to_sells")
+#     trashes_to_products = relationship("Trashes", back_populates="products_to_trashes")
 
-class DaySells(Base):
-    __tablename__= "DaySells"
-    id = Column(Integer, primary_key=True, index=True)
-    date = Column(DateTime)
-    # date = Column(String, default=datetime.date.today().strftime("%d%m%Y"))
-    # find a way to transform date in : dayoftheweek, dayofthemonth, monthoftheyear, publicholidays, season
+# class DaySells(Base):
+#     __tablename__= "DaySells"
+#     id = Column(Integer, primary_key=True, index=True)
+#     date = Column(DateTime)
+#     # date = Column(String, default=datetime.date.today().strftime("%d%m%Y"))
+#     # find a way to transform date in : dayoftheweek, dayofthemonth, monthoftheyear, publicholidays, season
 
-    day_to_sells = relationship("ProductSells")
+#     day_to_sells = relationship("ProductSells")
 
-class ProductSells(Base):
-    __tablename__ = "ProductSells"
-    id = Column(Integer, primary_key=True, index=True)
-    DaySellId = Column(Integer, ForeignKey("DaySells.id"))
-    ProductId = Column(Integer, ForeignKey("Products.id"))
-    quantity = Column(Float)
-    # "discount" could be great
+# class ProductSells(Base):
+#     __tablename__ = "ProductSells"
+#     id = Column(Integer, primary_key=True, index=True)
+#     DaySellId = Column(Integer, ForeignKey("DaySells.id"))
+#     ProductId = Column(Integer, ForeignKey("Products.id"))
+#     quantity = Column(Float)
+#     # "discount" could be great
 
-    products_to_sells = relationship("Products", back_populates="sells_to_products")
+#     products_to_sells = relationship("Products", back_populates="sells_to_products")
 
 
-class Trashes(Base):
-    __tablename__ = "Trashes"
-    id = Column(Integer, primary_key=True, index=True)
-    date = Column(DateTime)
-    rehabilitate = Column(Boolean, default=False)
-    gift = Column(Boolean, default=False)
-    ProductId = Column(Integer, ForeignKey("Products.id"))
-    quantity = Column(Float)
-    dateProd = Column(DateTime)
+# class Trashes(Base):
+#     __tablename__ = "Trashes"
+#     id = Column(Integer, primary_key=True, index=True)
+#     date = Column(DateTime)
+#     rehabilitate = Column(Boolean, default=False)
+#     gift = Column(Boolean, default=False)
+#     ProductId = Column(Integer, ForeignKey("Products.id"))
+#     quantity = Column(Float)
+#     dateProd = Column(DateTime)
 
-    products_to_trashes = relationship("Products", back_populates="trashes_to_products")
+#     products_to_trashes = relationship("Products", back_populates="trashes_to_products")

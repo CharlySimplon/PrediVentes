2208acf1f2452772542061db9efb64a4da0c15f5
Charly <beaussart@protonmail.com>
2022-08-30 09:38:47 +0200

    without sqldb

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import uvicorn
 from fastapi import FastAPI, APIRouter, Request
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
-from sqldb import crud, models, schemas
-from sqldb.database import SessionLocal, engine
+# from sqldb import crud, models, schemas
+# from sqldb.database import SessionLocal, engine
 
 app = FastAPI()
 # app.add_middleware(HTTPSRedirectMiddleware)
@@ -12,7 +12,7 @@ api_router = APIRouter()
 
 @api_router.get("/")
 async def root():
-    return {"message": "TEST "}
+    return {"message": "TEST 2 "}
 
 app.include_router(api_router)
 

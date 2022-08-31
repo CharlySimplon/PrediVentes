061a7060621364254d8cde28556afcc587e7d3c8
Charly <beaussart@protonmail.com>
2022-08-31 10:04:33 +0200

    test azuree

--- sqldb/database.py
+++ sqldb/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-import psycopg2
-from dotenv import load_dotenv
-load_dotenv(override=True)
+# import os
+# import psycopg2
+# from dotenv import load_dotenv
+# load_dotenv(override=True)
 
 
 # SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace('postgres://','postgresql://')

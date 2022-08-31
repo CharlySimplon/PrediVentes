0be2a1a90e8e17fd22bce20c64d79f0b9a7414db
Charly <beaussart@protonmail.com>
2022-08-31 11:27:32 +0200

    propre

--- sqldb/database.py
+++ sqldb/database.py
@@ -2,16 +2,8 @@ from sqlalchemy import create_engine, engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-# import psycopg2
-# from dotenv import load_dotenv
-# load_dotenv(override=True)
-
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
-
-
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123soleil@localhost:5432/leclerc_test"
-# SQLALCHEMY_DATABASE_URL = "postgresql://WelleatAdminLeclerc:Gaudec42@leclerc-welleat.postgres.database.azure.com:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
